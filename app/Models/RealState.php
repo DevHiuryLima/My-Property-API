904c3a82607ec8c0c19eca41fc81d069d502a38c
Hiury Lima <limahiury6@gmail.com>
2023-10-12 11:50:21 -0300

    Iniciando EndPoint

---
+++ app/Models/RealState.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RealState extends Model
+{
+    use HasFactory;
+
+    protected $table = 'real_state'; // Por convenção no Laravel o nome das tabela é no plural
+    public function user()
+    {
+        return$this->belongsTo(User::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('Api')->group(function (){
+
+    Route::prefix('real-states')->name('real_states.')->group(function (){
+        Route::get('/', [\App\Http\Controllers\Api\RealStateController::class, 'index'])->name('index'); // api/v1/real-states/
+    });
+});
