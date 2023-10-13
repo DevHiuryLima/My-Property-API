e0501e5931ac29d16fd598f58f3280d49621b2d4
Hiury Lima <limahiury6@gmail.com>
2023-10-13 14:44:51 -0300

    Mapeando Relação.

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(RealState::class);
     }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
 }
