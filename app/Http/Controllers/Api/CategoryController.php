9b33b17414f578cc5b29d4ea990bf1a45b0fcffc
Hiury Lima <limahiury6@gmail.com>
2023-10-13 14:25:45 -0300

    Recuperando Imvóveis por categoria.

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -120,4 +120,18 @@ x     * @return \Illuminate\Http\Response
             return response()->json($message->getMessage(), 401);
         }
     }
+
+    public function realState($id)
+    {
+        try {
+            $category = $this->category->findOrFail($id);
+
+            return response()->json([
+                'data' => $category->realStates,
+            ], 200);
+        } catch (\Exception $e) {
+            $message = new ApiMessages($e->getMessage());
+            return response()->json($message->getMessage(), 401);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::prefix('v1')->group(function (){
     });
 
     Route::name('categories.')->group(function (){
+        Route::get('categories/{id}/real-states', [CategoryController::class, 'realState']);
         Route::resource('categories', CategoryController::class);
     });
 });
