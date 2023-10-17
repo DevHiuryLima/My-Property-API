00ff348b3a8cdb4d71edbb20f6a34ca0f03fbb7b
Hiury Lima <limahiury6@gmail.com>
2023-10-17 15:44:11 -0300

    Relação entre Models de Localidade.

---
+++ app/Models/Address.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city()
+    {
+        return$this->belongsTo(City::class);
+    }
+}
---
+++ app/Models/City.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function adresses()
+    {
+        return $this->hasMany(Address::class);
+    }
+}
