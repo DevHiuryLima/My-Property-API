1073e72cb9f8cdc2209891665686ffca813b23bc
Hiury Lima <limahiury6@gmail.com>
2023-10-17 09:52:18 -0300

    Validações no Login.

--- app/Http/Controllers/Api/Auth/LoginJwtController.php
+++ app/Http/Controllers/Api/Auth/LoginJwtController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Api\ApiMessages;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginJwtController extends Controller
 {
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
+
+        Validator::make($credentials, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ])->validate();
 
         if(!$token = auth('api')->attempt($credentials)) {
             $message = new ApiMessages('Unauthorized');
